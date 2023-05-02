ab4e91bac7feaf959b082127c2d1ca971a523d44
paksk-pcs <131220105+paksk-pcs@users.noreply.github.com>
2023-05-02 17:41:49 +0800

    testing

--- src/test_app.py
+++ src/test_app.py
@@ -9,7 +9,7 @@ def client():
 def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Welcome to my Flask app" in response.data
+    assert b"Hello, World!" in response.data
 
 def test_about_page(client):
     response = client.get('/about')
